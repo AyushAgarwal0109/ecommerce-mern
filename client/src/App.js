91d7bca13b021881c8ecd12adf50a05071f08504
Ayush Agarwal <agarwal.ayush.0109@gmail.com>
2022-02-01 16:54:05 +0530

    Product and Rating

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,24 @@
 import { Container } from 'react-bootstrap';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Footer from './components/Footer';
 import Header from './components/Header';
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
 
 const App = () => {
   return (
-    <>
+    <Router>
       <Header />
       <main className='py-3'>
         <Container>
-          <h1>Hello World.</h1>
+          <Routes>
+            <Route path='/' element={<HomeScreen />} />
+            <Route path='/product/:id' element={<ProductScreen />} />
+          </Routes>
         </Container>
       </main>
       <Footer />
-    </>
+    </Router>
   );
 };
 
